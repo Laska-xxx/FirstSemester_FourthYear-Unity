a235235ee83dcc8161046682600ea05d7f0e9051
Laska-xxx <ZubavichusEYa23@st.ithub.ru>
2026-09-16 02:45:49 +0300

    feat: wall

---
+++ TestTask/Assets/_Sourse/Environment/Wall.cs
@@ -0,0 +1,14 @@
+using Player;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class Wall : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.TryGetComponent<PlayerController>(out var player))
+        {
+            player.Die();
+        }
+    }
+}
