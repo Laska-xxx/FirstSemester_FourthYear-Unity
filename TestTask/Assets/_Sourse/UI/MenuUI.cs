90ed391eef3559adb36258ba304e2c3dd95da5fd
Laska-xxx <ZubavichusEYa23@st.ithub.ru>
2026-09-16 05:50:02 +0300

    feat: Menu View

---
+++ TestTask/Assets/_Sourse/UI/MenuUI.cs
@@ -0,0 +1,30 @@
+using GameState;
+using UnityEngine;
+using Zenject;
+
+public class MenuUI : MonoBehaviour
+{
+    [SerializeField] private GameObject _menuPanel;
+
+    private StateMachine _stateMachine;
+
+    [Inject] private void Init(StateMachine stateMachine)
+    {
+        _stateMachine = stateMachine;
+    }
+
+    private void OnEnable()
+    {
+        _stateMachine.OnStateChanged += OnStateChanged;
+    }
+
+    private void OnDisable()
+    {
+        _stateMachine.OnStateChanged -= OnStateChanged;
+    }
+
+    private void OnStateChanged(BaseState newState)
+    {
+        _menuPanel.SetActive(newState is MenuState);
+    }
+}
